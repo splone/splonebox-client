f1182bab711c75d25e972cf699081b13b47d8e73
mkind <sascha.zinke@splone.com>
2016-03-02 16:27:10 +0100

    adding LGPLv3 License text in order to fix issue #12

--- Splonecli/Api/result.py
+++ Splonecli/Api/result.py
@@ -1,3 +1,22 @@
+"""
+This file is part of the splonebox python client library.
+
+The splonebox python client library is free software: you can
+redistribute it and/or modify it under the terms of the GNU Lesser
+General Public License as published by the Free Software Foundation,
+either version 3 of the License or any later version.
+
+It is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this splonebox python client library.  If not,
+see <http://www.gnu.org/licenses/>.
+
+"""
+
 import logging
 from threading import Event
 
