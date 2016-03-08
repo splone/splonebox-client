05a1ebee50b2530cf27aa8e2f7c59034cdafd19f
ben <benjohnwie@gmail.com>
2016-03-08 12:30:09 +0100

    add minimal setup.py see #5

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Splonebox Python Client',
+      version='0.1',
+      description='A client implementation to communicate with the splonebox core',
+      packages=['splonecli', 'splonecli.api', 'splonecli.rpc'])
