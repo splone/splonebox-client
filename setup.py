6061738a53e0d1f0899072a250e05fbd78a718e6
mkind <sascha.zinke@splone.com>
2016-04-12 17:40:54 +0200

    adding dependencies in setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ from distutils.core import setup
 setup(name='Splonebox Python Client',
       version='0.1',
       description='A client implementation to communicate with the splonebox core',
+      install_requires=['pycrypto>=2.6.1', 'msgpack-python>=0.4.6'],
       packages=['splonecli', 'splonecli.api', 'splonecli.rpc', 'splonecli.os'])
