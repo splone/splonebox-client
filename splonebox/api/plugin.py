744dfedbc227dfad6fd8b1ae56f707ee81d0b8fd
ben <benjohnwie@gmail.com>
2016-05-17 14:01:34 +0200

    add result handling to msgpack-rpc layer

--- splonebox/api/plugin.py
+++ splonebox/api/plugin.py
@@ -55,6 +55,7 @@ class Plugin:
         self._rpc = MsgpackRpc()
         # register run function @ rpc dispatcher
         self._rpc.register_function(self._handle_run, "run")
+        self._rpc.register_function(self._handle_result, "result")
 
         # pending_responses
         self._responses_pending = {int: Result()}  # msgid: result
